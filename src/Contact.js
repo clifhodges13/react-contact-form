814ca927cd4f41547e8c4e4acf260e58cc0d0524
Clif <clifhodges13@gmail.com>
2019-07-02 13:53:06 -0500

    updated app URL in axios post Contact.js

--- src/Contact.js
+++ src/Contact.js
@@ -25,7 +25,7 @@ class Contact extends Component {
       message: this.state.message
     }
 
-    axios.post('API_URI', data)
+    axios.post('https://create-react-app-kyd78jo8t.now.sh/', data)
       .then(res => {
         this.setState({
           sent: true
